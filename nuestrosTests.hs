914c0defa9b2903edafa4295f2002054389b4c4a
SUPERNOOB20 <82103347+SUPERNOOB20@users.noreply.github.com>
2023-05-14 03:58:17 -0300

    copypastea RobertoCarlos de prueba.hs y arregla sintaxis de todo lo demás

--- nuestrosTests.hs
+++ nuestrosTests.hs
@@ -3,7 +3,7 @@ import Solucion
 
 -- Casos de Test
 
-main = run1 ++ run2 ++ run3 ++ run4 ++ run5 ++ run6 ++ run7 ++ run8 ++ run9 ++ run10
+main = [run1] ++ [run2] ++ [run3] ++ [run4] ++ [run5] ++ [run6] ++ [run7] ++ [run8] ++ [run9] ++ [run10]
 
 run1 = runTestTT testEjercicio1
 run2 = runTestTT testEjercicio2
@@ -22,47 +22,52 @@ run10 = runTestTT testEjercicio10
 -- Esquema de los test suites: https://docs.google.com/spreadsheets/d/1ISs0Z0TMPZ_7Ro_69iO2XR96wovZvKPW/edit#gid=1370724871
 
 testEjercicio1 = test [
-    "nombresDeUsuarios 1: Lista de usuarios vacía"         ~: ([],[],[])                 ~?= [],
-    "nombresDeUsuarios 2: Lista de usuarios con repetidos" ~: (["u2", "u3", "u2"],[],[]) ~?= ["Mauri", "Andy"],
-    "nombresDeUsuarios 3: Lista de usuarios sin repetidos" ~: redA                       ~?= ["Tomi", "Mauri", "Andy"],
+    "nombresDeUsuarios 1: Lista de usuarios vacía"         ~: nombresDeUsuarios ([],[],[])                 ~?= [],
+    "nombresDeUsuarios 2: Lista de usuarios con repetidos" ~: nombresDeUsuarios (["u2", "u3", "u2"],[],[]) ~?= ["Mauri", "Andy"],
+    "nombresDeUsuarios 3: Lista de usuarios sin repetidos" ~: nombresDeUsuarios redA                       ~?= ["Tomi", "Mauri", "Andy"]
     ]
 
 testEjercicio2 = test [
-    "amigosDe 1: u no tiene amigos" ~: redB u4 ~?= [],
-    "amigosDe 2: u tiene amigos CON nombres repetidos ∧ IDs distintos" ~: ([(1, "Tomi"), (2, "u"), (727, "Tomi")],[(2, u), (1,"Tomi")],[(2, u), (727,"Tomi")], []) ~?= [(1, "Tomi"),(727, "Tomi")],
-    "amigosDe 3: u tiene amigos, pero SIN nombres repetidos" ~: redA u3 ~?= [2, "Mauri"],
+    "amigosDe 1: u no tiene amigos" ~: amigosDe redB u4 ~?= [],
+    "amigosDe 2: u tiene amigos CON nombres repetidos ∧ IDs distintos" ~: amigosDe ([(1, "Tomi"), (2, "u"), (727, "Tomi")],[(2, u), (1,"Tomi")],[(2, u), (727,"Tomi")], []) ~?= [(1, "Tomi"),(727, "Tomi")],
+    "amigosDe 3: u tiene amigos, pero SIN nombres repetidos" ~: amigosDe redA u3 ~?= [2, "Mauri"]
     ]
     
 testEjercicio3 = test [
-    "cantidadDeAmigos 1: u no tiene amigos :("                                                             ~: redB u4 ~?= 0,
-    "cantidadDeAmigos 2: u tiene un solo amigo"                                                            ~: redA u1 ~?= 1,
-    "cantidadDeAmigos 3: u tiene más (mayor estricto) de un amigo (tiene una cantidad finita n de amigos)" ~: redA u2 ~?= 2,
+    "cantidadDeAmigos 1: u no tiene amigos :("                                                             ~: cantidadDeAmigos redB u4 ~?= 0,
+    "cantidadDeAmigos 2: u tiene un solo amigo"                                                            ~: cantidadDeAmigos redA u1 ~?= 1,
+    "cantidadDeAmigos 3: u tiene más (mayor estricto) de un amigo (tiene una cantidad finita n de amigos)" ~: cantidadDeAmigos redA u2 ~?= 2
 ]
 
 testEjercicio4 = test [
-    "usuarioConMasAmigos 1: La red tiene un solo usuario y ninguna relación." ~: redB     ~?= u4,
-    "usuarioConMasAmigos 2: Dos usuarios tienen la misma cantidad de amigos"  ~: redC     ~?= u1 || u2 || u3,
-    "usuarioConMasAmigos 3: Un usuario tiene una cantidad de amigos mayor estricta a la cantidad de amigos de los demás usuarios" ~: redA ~?= u2,
+    "usuarioConMasAmigos 1: La red tiene un solo usuario y ninguna relación." ~: usuarioConMasAmigos redB ~?= u4,
+    "usuarioConMasAmigos 2: Dos usuarios tienen la misma cantidad de amigos"  ~: usuarioConMasAmigos redC ~?= u1 || u2 || u3,
+    "usuarioConMasAmigos 3: Un usuario tiene una cantidad de amigos mayor estricta a la cantidad de amigos de los demás usuarios" ~: usuarioConMasAmigos redA ~?= u2
 ]
-   
+
+
+{-
+Para simplificar el testing utilizaremos estaRobertoCarlosTesteable4, que remplaza el 1000000 de amigos requeridos
+en el enunciado por un número más manejable. En nuestro caso, utilizaremos al 4.
+-}
 testEjercicio5 = test [
 -- NOTAR QUE se cambió el 1000000 por el 5
-    "estaRobertoCarlos 1: cantidadDeAmigos > 5" ~: redR1  ~?= True,
-    "estaRobertoCarlos 2: cantidadDeAmigos = 5" ~: redR2 ~?= False,
-    "estaRobertoCarlos 3: cantidadDeAmigos < 5" ~: redA ~?= False,
+    "estaRobertoCarlos 1: cantidadDeAmigos > 4" ~: estaRobertoCarlosTesteable4 redR1 ~?= True,
+    "estaRobertoCarlos 2: cantidadDeAmigos = 4" ~: estaRobertoCarlosTesteable4 redR2 ~?= False,
+    "estaRobertoCarlos 3: cantidadDeAmigos < 4" ~: estaRobertoCarlosTesteable4 redA ~?= False
 ]
    
 testEjercicio6 = test [
-    "publicacionesDe 1: u no tiene publicaciones"                        ~: redB u4 ~?= [],
-    "publicacionesDe 2: u tiene una sola publicación"                    ~: redC u1 ~?= [pub1_1],
-    "publicacionesDe 3: u tiene más (mayor estricto) de una publicación" ~: redA u3 ~?= [pub3_1, pub3_2, pub3_3],
+    "publicacionesDe 1: u no tiene publicaciones"                        ~: publicacionesDe redB u4 ~?= [],
+    "publicacionesDe 2: u tiene una sola publicación"                    ~: publicacionesDe redC u1 ~?= [pub1_1],
+    "publicacionesDe 3: u tiene más (mayor estricto) de una publicación" ~: publicacionesDe redA u3 ~?= [pub3_1, pub3_2, pub3_3]
 ]
    
 testEjercicio7 = test [
     "publicacionesQueLeGustanA 1: A u no le gusta ninguna publicación" ~:     ~?= [],
     "publicacionesQueLeGustanA 2: (∀n>=2) (Pertenece u, pub_n[1]) ∧ (pub_n[1] = pub_n-1[1])" ~:        ~?=   ,                             -- Pendiente: formalizar la descripción de este caso (¿o alcanza con este grado de formalidad?)
-    "publicacionesQueLeGustanA 3: Hay publicaciones con un mismo autor que le gustan a u" ~:      ~?=     ,
-   -- DEPRECATED: "publicacionesQueLeGustanA 4: No hay publicaciones repetidas entre las publicaciones que le gustan a u" ~:      ~?=     ,
+    "publicacionesQueLeGustanA 3: Hay publicaciones con un mismo autor que le gustan a u" ~:      ~?=
+   -- DEPRECATED: "publicacionesQueLeGustanA 4: No hay publicaciones repetidas entre las publicaciones que le gustan a u" ~:      ~?=
 ]
    
 testEjercicio8 = test [
@@ -72,7 +77,7 @@ testEjercicio8 = test [
     "lesGustanLasMismasPublicaciones 4: Hay algunos (pero no todos) likes en comùn entre ambos usuarios" ~:      ~?= False,
     "lesGustanLasMismasPublicaciones 5: Los likes de un usuario están contenidos en los likes del otro usuario (pero no son iguales)" ~:      ~?= False,
     "lesGustanLasMismasPublicaciones 6: NO hay likes en comùn entre ambos usuarios" ~:       ~?= False,
-    "lesGustanLasMismasPublicaciones 7: Los dos usuarios son el mismo (u1 = u2)" ~:        ~?= True,
+    "lesGustanLasMismasPublicaciones 7: Los dos usuarios son el mismo (u1 = u2)" ~:        ~?= True
 ]
    
 testEjercicio9 = test [
@@ -81,14 +86,14 @@ testEjercicio9 = test [
     "tieneUnSeguidorFiel 3: (∃u2 : Usuario) (Pertenece(u2, usuarios(red)) ∧ u ̸= u2 ∧ (∀pub : Publicacion) (Pertenece(pub, publicaciones(red)) ∧ usuarioDePublicacion(pub) Pertenece(u2, likesDePublicacion(pub))= u " ~:      ~?= True,
     -- DEPRECATED: "tieneUnSeguidorFiel 4: (∃u2 : Usuario) (Pertenece(u2, usuarios(red)) ∧ u ̸= u2 ∧ (∀pub : Publicacion) (Pertenece(pub, publicaciones(red)) ∧ usuarioDePublicacion(pub) ¬Pertenece(u2, likesDePublicacion(pub))= u " ~:      ~?= False,
     "tieneUnSeguidorFiel 4: Existe un usuario u2 al que le gustan algunas publicaciones de u, y no le gusta ninguna otra publicación" ~:        ~?=   False,
-    "tieneUnSeguidorFiel 5: Existe un usuario u2 al que le gustan algunas publicaciones de u, y tambièn le gustan otras publicaciones de otros usuarios (distintos a u y u2)" ~:        ~?=   False,
+    "tieneUnSeguidorFiel 5: Existe un usuario u2 al que le gustan algunas publicaciones de u, y tambièn le gustan otras publicaciones de otros usuarios (distintos a u y u2)" ~:        ~?=   False
 ]
    
 testEjercicio10 = test [
     "existeSecuenciaDeAmigos 1: u1 = u2" ~:     ~?= False,
     "existeSecuenciaDeAmigos 2: u1 y u2 son amigos" ~:        ~?= True,
     "existeSecuenciaDeAmigos 3: u1 y u2 NO son amigos ∧ (∃us : seq⟨Usuario⟩)(CadenaDeAmigos(us, red))" ~:      ~?= True,
-    "existeSecuenciaDeAmigos 4: ¬(∃us : seq⟨Usuario⟩)(CadenaDeAmigos(us, red))" ~:       ~?= False,
+    "existeSecuenciaDeAmigos 4: ¬(∃us : seq⟨Usuario⟩)(CadenaDeAmigos(us, red))" ~:       ~?= False
 ]
 
 
@@ -130,5 +135,5 @@ redA = ([u1, u2, u3], [rel1_2, rel2_3], [pub1_1, pub1_2, pub1_3, pub2_1, pub2_2,
 redB = ([u4], [], [])
 redC = ([u1, u2, u3], [rel1_2, rel1_3, rel2_3], [pub1_1, pub2_1, pub2_2, pub3_1, pub3_2, pub3_3])
 
-redR1 = ([(1000, Roberto Carlos), u1, u2, u3, u4, u5, u6, u7], [(1000, "Roberto Carlos", u1), (1000, "Roberto Carlos", u2), (1000, "Roberto Carlos", u3), (1000, "Roberto Carlos", u4), (1000, "Roberto Carlos", u5), (1000, "Roberto Carlos", u6), (1000, "Roberto Carlos", u7)], [])
-redR2 = ([(1000, Roberto Carlos), u1, u2, u3, u4, u5], [(1000, "Roberto Carlos", u1), (1000, "Roberto Carlos", u2), (1000, "Roberto Carlos", u3), (1000, "Roberto Carlos", u4), (1000, "Roberto Carlos", u5)], [])
+redR1 = ([(1000, Roberto Carlos), u1, u2, u3, u4, u5, u6], [(1000, "Roberto Carlos", u1), (1000, "Roberto Carlos", u2), (1000, "Roberto Carlos", u3), (1000, "Roberto Carlos", u4), (1000, "Roberto Carlos", u5), (1000, "Roberto Carlos", u6)], [])
+redR2 = ([(1000, Roberto Carlos), u1, u2, u3, u4], [(1000, "Roberto Carlos", u1), (1000, "Roberto Carlos", u2), (1000, "Roberto Carlos", u3), (1000, "Roberto Carlos", u4)], [])
